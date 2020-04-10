6fa430e5450174ee8550c2c421933d46fb758b36
SomitKhakse <63151864+SomitKhakse@users.noreply.github.com>
2020-04-11 03:41:22 +0800

    Created as an test assignment for MyCaptain.

---
+++ LetsCode1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letscode1;
+
+import java.util.Scanner;
+
+
+
+
+/**
+ *
+ * @author M N Khakse
+ */
+public class LetsCode1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Enter the number of minutes to convert:");
+        int mins;
+        Scanner s= new Scanner(System.in);
+        mins= s.nextInt();
+        int TotalDays= (366*mins)/527040;
+        System.out.println(mins+" minutes is approx. "+TotalDays/365+" years and "+TotalDays%365+" days");
+         
+        
+        
+    }
+    
+}
